c5469e691aba87ba2be11286e883e7238c1d746b
klayhung <klayhung@gmail.com>
2025-02-12 18:57:36 +0800

    feat: 將 CreditCard 列舉型別轉換為抽象類別，並為每個類型新增折扣屬性

--- CreditCard.cs
+++ CreditCard.cs
@@ -1,14 +1,41 @@
 namespace SmartEnum;
 
-public class CreditCard : Enumeration<CreditCard>
+public abstract class CreditCard : Enumeration<CreditCard>
 {
-    public static readonly CreditCard Standard = new(1, "Standard");
-    public static readonly CreditCard Premium = new(2, "Premium");
-    public static readonly CreditCard Platinum = new(3, "Platinum");
+    public static readonly CreditCard Standard = new StandardCreditCard();
+    public static readonly CreditCard Premium = new PremiumCreditCard();
+    public static readonly CreditCard Platinum = new PlatinumCreditCard();
 
     private CreditCard(int value, string name) : base(value, name)
     {
     }
 
-    public double Discount => 0.0;
+    public abstract double Discount { get; }
+
+    public sealed class StandardCreditCard : CreditCard
+    {
+        public StandardCreditCard() : base(value: 1, name: "Standard")
+        {
+        }
+
+        public override double Discount => 0.01;
+    }
+
+    public sealed class PremiumCreditCard : CreditCard
+    {
+        public PremiumCreditCard() : base(value: 2, name: "Premium")
+        {
+        }
+
+        public override double Discount => 0.05;
+    }
+
+    public sealed class PlatinumCreditCard : CreditCard
+    {
+        public PlatinumCreditCard() : base(value: 3, name: "Platinum")
+        {
+        }
+
+        public override double Discount => 0.1;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 using SmartEnum;
 
 var creditCard = CreditCard.FromValue(1);
+var premium = CreditCard.FromName("Premium");
 
 
 Console.WriteLine($"Credit card: {creditCard}, discount: {creditCard.Discount:P}");
+Console.WriteLine($"Credit card: {premium}, discount: {premium.Discount:P}");
 Console.ReadKey();
