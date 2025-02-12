d95f999953ce360b0d8b6906afc365d2bd10734a
klayhung <klayhung@gmail.com>
2025-02-12 14:09:22 +0800

    feat: 初版 Enum 使用方式

---
+++ CreditCard.cs
@@ -0,0 +1,8 @@
+namespace SmartEnum;
+
+internal enum CreditCard
+{
+    Standard = 1,
+    Premium = 2,
+    Platinum = 3,
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using SmartEnum;
+
+var creditCard = CreditCard.Platinum;
+
+var discount = creditCard switch
+{
+    CreditCard.Standard => 0.01,
+    CreditCard.Premium => 0.05,
+    CreditCard.Platinum => 0.1,
+    _ => throw new ArgumentOutOfRangeException(nameof(creditCard), creditCard, null)
+};
+
+Console.WriteLine($"Credit card: {creditCard}, discount: {discount:P}");
+Console.ReadKey();
